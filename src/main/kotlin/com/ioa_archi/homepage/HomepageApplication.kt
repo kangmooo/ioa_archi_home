9fbc990eae1838700691eb601affadb7ff8315c7
sungtaekang <sungtae.kang@caring.co.kr>
2025-06-10 18:38:50 +0900

    init

---
+++ src/main/kotlin/com/ioa_archi/homepage/HomepageApplication.kt
@@ -0,0 +1,11 @@
+package com.ioa_archi.homepage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomepageApplication
+
+fun main(args: Array<String>) {
+    runApplication<HomepageApplication>(*args)
+}
---
+++ src/test/kotlin/com/ioa_archi/homepage/HomepageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ioa_archi.homepage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomepageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
